5dac1bf0c645f43fdeb69c2c8bbf3e1ac9992241
Nahid1911 <nahid.raihan@gmail.com>
2023-09-28 09:47:18 +0600

    Create React app

---
+++ src/components/Greetings.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+
+function Greetings() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/greetings/get_message').then((response) => {
+      setMessage(response.data.message);
+    }).catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  }, []);
+
+  return (
+<div>
+<h1>Hello World</h1><br/>
+<p>{message}</p>
+</div>
+  );
+}
+
+export default Greetings
